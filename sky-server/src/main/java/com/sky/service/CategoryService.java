aeced951f706b3391af2055047ab10137f06843c
cumin1 <3144941746@qq.com>
2025-05-02 12:47:03 +0800

    实现分类分页查询接口

---
+++ sky-server/src/main/java/com/sky/service/CategoryService.java
@@ -0,0 +1,15 @@
+package com.sky.service;
+
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.result.PageResult;
+import org.springframework.stereotype.Service;
+
+public interface CategoryService {
+
+    /**
+     * 分类分页查询
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    PageResult selectByPage(CategoryPageQueryDTO categoryPageQueryDTO);
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,33 @@
+package com.sky.service.impl;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import com.sky.mapper.CategoryMapper;
+import com.sky.result.PageResult;
+import com.sky.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    /**
+     * 分页查询分类表
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    @Override
+    public PageResult selectByPage(CategoryPageQueryDTO categoryPageQueryDTO) {
+        PageHelper.startPage(categoryPageQueryDTO.getPage(),categoryPageQueryDTO.getPageSize());
+        Page<Category> page = categoryMapper.selectByPage(categoryPageQueryDTO);
+        PageResult pageResult = new PageResult();
+        pageResult.setTotal(page.getTotal());
+        pageResult.setRecords(page.getResult());
+        return pageResult;
+    }
+}
